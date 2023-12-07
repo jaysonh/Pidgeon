15235fd84a5ef3173c082ae8efaacbbc034f0d0e
Jayson Haebich <jaysonkh@gmail.com>
2023-12-07 15:20:16 +0800

    added http get sensor

---
+++ python/DeviceInGetHttp.py
@@ -0,0 +1,49 @@
+# {
+#    "name" : "Siggrow 44192",
+#    "id"   : "00044192",
+#    "type" : "httpGet",
+#    "requestAddr" : "https://app.sigrow.com/api/v2/remote/44192/data",
+#    "apiKey" : "c61e7d20-3ab1-43f9-ad32-b04e3fd9a822"
+# }
+
+import json
+from MQTTHandler import MQTTHandler
+from SensorControl import *
+import urllib.request
+
+class DeviceInGetHttp(SensorControl):
+    
+    def __init__(self, json_data : json):
+        super().__init__( json_data )
+        print("creating sensor Rest HTTP")
+        
+        self.request_addr = json_data["requestAddr"]
+        self.api_key = json_data["apiKey"]
+
+        self.update()
+
+        pass
+
+    def update(self):       
+                
+        url = self.request_addr
+        headers={"x-api-key" : self.api_key}
+        response = requests.get(self.request_addr, headers=headers)
+
+        resultJson = response.json()
+        print( resultJson )
+
+        pass
+
+    def on_message(client, userdata, msg):
+        #print(f"Message received [{msg.topic}]: {msg.payload}")
+        pass
+
+    def getData(self) ->float :
+        pass
+    
+    def sendData(self, v : float):
+        self.value = v
+        #print(f"sendData MQTT: {v}")
+        #self.mqtt.send_msg( self.topic, str(v) )
+        pass
--- python/Sensor.py
+++ python/Sensor.py
@@ -2,6 +2,7 @@ import json
 from typing import List
 from MQTTHandler import *
 from SensorMQTT import *
+from DeviceInGetHttp import *
 
 class Sensor:
 
@@ -10,6 +11,8 @@ class Sensor:
         # MQTT Type takes a request on an MQTT address and sends the result back on another address
         if( self.type == "mqtt"):
             self.device = SensorMQTT(json_data  )
+        if( self.type == "httpGet"):
+            self.device = DeviceInGetHttp(json_data  )
         else:
             print("Error invalid sensor type")
         pass
